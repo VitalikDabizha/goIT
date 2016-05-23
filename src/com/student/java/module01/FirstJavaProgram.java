f831e9cce2bc5a5bba46c867b3efa2b06492a11e
Vitalik <seo20013@gmail.com>
2016-05-24 01:50:29 +0300

    Initial commi of the project

---
+++ src/com/student/java/module01/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+package com.student.java.module01;
+
+public class FirstJavaProgram {
+    public static void main( String[] args){
+    System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
