f1e96395cc5d763748b60654fa15ca9656e7f19b
Vitalik <seo20013@gmail.com>
2016-06-14 13:52:52 +0300

    Dz-mod5

---
+++ src/com/student/java/module05/task1/ArrayMinMaxSort.java
@@ -0,0 +1,7 @@
+package com.student.java.module05.task1;
+
+/**
+ * Created by vitalik on 14.06.16.
+ */
+public class ArrayMinMaxSort {
+}
---
+++ src/com/student/java/module05/task1/Runner.java
@@ -0,0 +1,17 @@
+package com.student.java.module05.task1;
+
+public class Runner {
+
+    public static void main(String[] args){
+
+    Ar ewe = new Ar();
+
+
+
+
+    }
+
+
+
+
+}
