76beb9bbf523ff6d3a349a176cf6041c475e38f7
Vitalik <seo20013@gmail.com>
2016-06-14 13:55:29 +0300

    DzMod-5

--- src/com/student/java/module05/task1/ArrayMinMaxSort.java
+++ src/com/student/java/module05/task1/ArrayMinMaxSort.java
@@ -1,7 +1,76 @@
 package com.student.java.module05.task1;
 
-/**
- * Created by vitalik on 14.06.16.
- */
 public class ArrayMinMaxSort {
+
+    public static String getMax(int[] myArray) {
+
+        int max = myArray[0];
+
+        for (int i = 0; i < myArray.length; i++) {
+
+            if (max < myArray[i])
+
+            {
+                max = myArray[i];
+            }
+        }
+
+        return "Max number is: " + max;
+    }
+
+    public static String getMin(int[] myArray) {
+
+        int min = myArray[0];
+
+        for (int i = 0; i < myArray.length; i++) {
+
+            if (min > myArray[i])
+
+            {
+                min = myArray[i];
+            }
+        }
+
+        return "Min number is: " + min;
+
+    }
+
+    public static void sortArray(int[] array){
+
+        boolean swap = true;
+
+        while (swap){
+
+            swap = false;
+
+            for (int i = 0; i < array.length - 1; i++) {
+
+                if (array[i] > array[i + 1]) {
+
+                    int temp = array[i];
+
+
+                    array[i] = array[i + 1];
+
+                    array[i + 1] = temp;
+
+                    swap = true;
+
+                }
+            }
+        }
+
+        System.out.println("Sort results:");
+
+        for (int elem : array){
+
+            System.out.println(elem);
+        }
+
+    }
+
+
+
+
+
 }
--- src/com/student/java/module05/task1/Runner.java
+++ src/com/student/java/module05/task1/Runner.java
@@ -4,14 +4,14 @@ public class Runner {
 
     public static void main(String[] args){
 
-    Ar ewe = new Ar();
+        int [] arg = {3,7,34,51,9,1,2,5,63,24};
 
+        System.out.println(ArrayMinMaxSort.getMax(arg));
 
+        System.out.println(ArrayMinMaxSort.getMin(arg));
 
+        ArrayMinMaxSort.sortArray(arg);
 
     }
-
-
-
 
 }
